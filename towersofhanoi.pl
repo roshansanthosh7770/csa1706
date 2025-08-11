da8b7d885b26cca77d716e663b277204aa822408
roshansanthosh7770 <161292545+roshansanthosh7770@users.noreply.github.com>
2025-08-11 16:47:19 +0530

    Delete towersofhanoi.pl

--- towersofhanoi.pl
+++
@@ -1,9 +0,0 @@
-hanoi(1, Source, Target, _) :-
-    write('Move disk from '), write(Source),
-    write(' to '), write(Target), nl.
-hanoi(N, Source, Target, Aux) :-
-    N > 1,
-    M is N - 1,
-    hanoi(M, Source, Aux, Target),
-    hanoi(1, Source, Target, _),
-    hanoi(M, Aux, Target, Source).
